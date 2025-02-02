c44d6517bcf6ac6347d641b8bbceb70f07f57cfd
euning <52948715+eunee99@users.noreply.github.com>
2025-02-02 16:13:42 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ C++17/백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -2,15 +2,14 @@
 #include <cstring>
 using namespace std;
 
+int arr[26];
 int main(void) {
 	string s;
 	cin >> s;
-
-	for (char a = 'a';a <= 'z';a++) {
-		int cnt = 0;
-		for (auto c : s)
-			if (a == c) cnt++;
-		cout << cnt << ' ';
+	for (auto c : s)
+		arr[c - 97]++;
+	for(int i = 0;i<26;i++){
+		cout << arr[i] << ' ';
 	}
 	return 0;
 }
